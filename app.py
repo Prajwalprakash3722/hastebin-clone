9c3b4e276930787999b404e2ae8e531b6f81adf5
Prajwal P <prajwalprakash3722@gmail.com>
2021-09-06 16:08:28 +0530

    changes-3

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL').replace('postgres', 'postgresql')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
 
 
